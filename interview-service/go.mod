d384b2a8b9112ac3a3d0b68b2c006b7073908f13
timur <timur@MacBook-Air.local>
2025-08-15 20:22:44 +0500

    GetInterview function ready

--- interview-service/go.mod
+++ interview-service/go.mod
@@ -2,16 +2,4 @@ module interview
 
 go 1.21
 
-require (
-	github.com/steebchen/prisma-client-go v0.25.0
-	google.golang.org/grpc v1.58.0
-	google.golang.org/protobuf v1.31.0
-)
-
-require (
-	github.com/golang/protobuf v1.5.3 // indirect
-	golang.org/x/net v0.12.0 // indirect
-	golang.org/x/sys v0.10.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20230711160842-782d3b101e98 // indirect
-)
+require github.com/steebchen/prisma-client-go v0.47.0 // indirect
--- interview-service/internal/service/interview_service.go
+++ interview-service/internal/service/interview_service.go
@@ -1,7 +1,9 @@
 package service
 
 import (
+	"context"
 	"interview/db"
+	"log"
 )
 
 type InterviewService struct {
@@ -14,6 +16,34 @@ func NewInterviewService(db *db.PrismaClient) *InterviewService {
 	}
 }
 
-func (s *InterviewService) GetInterview(id string) {
+func (s *InterviewService) GetInterview(id string) (*db.InterviewModel, error) {
+	ctx := context.Background()
 	interview, err := s.db.Interview.FindUnique(db.Interview.ID.Equals(id)).Exec(ctx)
+
+	if err != nil {
+		log.Print(err)
+		return nil, err
+	}
+
+	return interview, nil
+}
+
+func (s *InterviewService) CreateInterview(interview *db.InterviewModel) (*db.InterviewModel, error) {
+	ctx := context.Background()
+	createdInterview, err := s.db.Interview.CreateOne(
+		db.Interview.CompanyName.Set(interview.CompanyName),
+		db.Interview.UserID.Set(interview.UserID),
+		db.Interview.IsCompleted.Set(interview.IsCompleted),
+		db.Interview.Time.Set(interview.Time),
+		db.Interview.Notes.Set(interview.Notes),
+		db.Interview.VacancyURL.Set(interview.VacancyURL),
+		db.Interview.MeetingURL.Set(interview.MeetingURL),
+	).Exec(ctx)
+
+	if err != nil {
+		log.Print(err)
+		return nil, err
+	}
+
+	return createdInterview, nil
 }
